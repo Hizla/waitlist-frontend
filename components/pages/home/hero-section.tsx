a2eb9fc12dd6a479ab3ceefbd414474256b00d5e
YusufcanY <playders03@gmail.com>
2024-10-24 11:59:10 +0200

    fix: updated github button

--- components/pages/home/hero-section.tsx
+++ components/pages/home/hero-section.tsx
@@ -23,8 +23,8 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import Link from "next/link";
 import Image from "next/image";
+import ShimmerButton from "@/components/ui/shimmer-button";
 
 const waitlistFormSchema = z.object({
   email: z.string().email(),
@@ -151,21 +151,29 @@ export default function HeroSection() {
           </form>
         </Form>
 
-        <div className="flex gap-2 mt-4">
-          <Button size="icon" variant="outline" asChild>
-            <Link href="https://github.com/hizla">
+        <div className="flex gap-2 mt-8">
+          <ShimmerButton
+            shimmerSize="0.1em"
+            shimmerColor="#FFC300"
+            className="shadow-2xl px-4 py-2"
+          >
+            <span className="flex whitespace-pre-wrap text-center text-xs font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 items-center font-mono gap-2">
+              <span className="hidden md:inline">Checkout our</span>Github
               <svg
-                width={24}
-                height={24}
+                width={20}
+                height={20}
                 role="img"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <title>GitHub</title>
-                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                <path
+                  fill="#fff"
+                  d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                />
               </svg>
-            </Link>
-          </Button>
+            </span>
+          </ShimmerButton>
         </div>
       </div>
       <BackgroundBeams />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,32 @@ const config: Config = {
       fontFamily: {
         ibm: ["var(--font-ibm-plex-mono)"],
       },
+      animation: {
+        "shimmer-slide":
+          "shimmer-slide var(--speed) ease-in-out infinite alternate",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+      },
+      keyframes: {
+        "shimmer-slide": {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+      },
     },
     container: {
       center: true,
